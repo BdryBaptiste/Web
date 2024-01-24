57b45a46bc753609f6f924bcf85786e56e40a3a5
BdryBaptiste <baptiste.bdry@gmail.com>
2024-01-24 21:02:43 +0100

    desfg

--- deploy/api/index.js
+++ deploy/api/index.js
@@ -35,7 +35,7 @@ db.sequelize.sync()
 
 require("./routes")(app);
 
-const PORT =  433;
+const PORT =  process.env.PORT || 433;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
